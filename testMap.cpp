d468d803e29338cc66869a6ec19f7017cb6f8715
Peyton Markus <150070019+pmark2047@users.noreply.github.com>
2026-06-16 19:19:03 -0600

    Added Files Via Upload

---
+++ testMap.cpp
@@ -0,0 +1,36 @@
+/***********************************************************************
+ * Header:
+ *    Test
+ * Summary:
+ *    Driver to test map.h
+ * Author
+ *    Br. Helfrich
+ ************************************************************************/
+
+#ifndef DEBUG
+#define DEBUG   // Remove this to skip the unit tests
+#endif // DEBUG
+
+#include "testSpy.h"       // for the spy unit tests
+#include "testPair.h"      // for the pair unit tests
+#include "testBST.h"       // for the BST unit tests
+#include "testMap.h"       // for the map unit tests
+int Spy::counters[] = {};
+
+/**********************************************************************
+ * MAIN
+ * This is just a simple menu to launch a collection of tests
+ ***********************************************************************/
+int main()
+{
+#ifdef DEBUG
+   // unit tests
+   TestSpy().run();
+   TestPair().run();
+   TestBST().run();
+   TestMap().run();
+#endif // DEBUG
+   
+   return 0;
+}
+
